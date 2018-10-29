aa7721e489186d8a3dd35ceaef66188282032719
Vladimir Randjelovic <vladimirmicko@gmail.com>
2018-10-29 15:25:31 +0100

    ObjectFactory corrections

--- SOAPClient/src/main/java/com/kubris/soapClient/data/ObjectFactory.java
+++ SOAPClient/src/main/java/com/kubris/soapClient/data/ObjectFactory.java
@@ -10,7 +10,7 @@ import javax.xml.namespace.QName;
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the com.demo.soapservice package. 
+ * generated in the com.kubris.soapClient.data package. 
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
@@ -28,7 +28,7 @@ public class ObjectFactory {
     private final static QName _GetSignResponse_QNAME = new QName("http://soapservice.demo.com/", "getSignResponse");
 
     /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.demo.soapservice
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.kubris.soapClient.data
      * 
      */
     public ObjectFactory() {
